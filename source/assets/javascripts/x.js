dd2717245322724747bfe39ab2568c90d0af17a7
kseniy <kseniy94-94@mail.ru>
2015-02-20 13:40:00 +0000

    new

--- source/assets/javascripts/x.js
+++ source/assets/javascripts/x.js
@@ -2,6 +2,16 @@
 $(function() {
   var resizeTimeout;
   frame();
+  
+  
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 30
+    });
+    
 });
 var resizeTimeout;
   function frame(){
